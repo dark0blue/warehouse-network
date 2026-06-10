d64d68f35a31b66a10a4606339d55c3503aa1c1d
dark0blue <arif_mehmedali@abv.bg>
2026-06-10 17:18:01 +0300

    Create product class

---
+++ product.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+class Product:
+    def __init__(
+        self,
+        name: str,
+        code: str,
+        weight_kg: float,
+        volume_m3: float,
+        exp_date: date,
+        category: str = "general",
+    ):
+        self.name = name
+        self.exp_date = exp_date
+        self.code = code
+        self.weight_kg = weight_kg
+        self.volume_m3 = volume_m3
+        self.category = category
+
+    def __repr__(self):
+        return f"Product(name={self.name}, code={self.code})"
